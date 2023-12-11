d772af2f45cd2236ca5454010e73f6d37eab8f67
Tuan Dau <tukauu@gmail.com>
2023-12-11 00:40:58 -0500

    add: hours wasted section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import DownArrows from "@/components/DownArrows";
 
 async function getKillCount() {
   // Using hardcoded number for now because I'm getting rate limited
@@ -26,9 +27,14 @@ function calculateDryRate(killCount: number) {
   return ((1 - 1 / 3000) ** killCount * 100).toFixed(2);
 }
 
+function calculateHoursWasted(killCount: number) {
+  return killCount / 70;
+}
+
 export default function Home() {
   const [killCount, setKillCount] = useState("...");
   const [noDropRate, setNoDropRate] = useState("...");
+  const [hoursWasted, setHoursWasted] = useState("...");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,21 +42,42 @@ export default function Home() {
       //@ts-ignore
       setKillCount(count);
       setNoDropRate(calculateDryRate(count));
+      const hoursAtMole = calculateHoursWasted(await count).toFixed(2);
+      //@ts-ignore
+      setHoursWasted(hoursAtMole);
     };
 
     fetchData();
   }, []);
 
   return (
-    <div className="flex flex-col justify-center w-full h-screen space-y-2">
-      <div className="flex justify-center mb-16">
-        <Image src="/baby_mole.webp" alt="" height={200} width={200} />
+    <div>
+      <div className="-mt-8 flex flex-col justify-between items-center text-center w-full h-screen">
+        <div></div>
+        <div>
+          <div className="flex justify-center mb-16">
+            <Image src="/baby_mole.webp" alt="" height={200} width={200} />
+          </div>
+          <div className="flex justify-center text-4xl">
+            Moo has a mole kill count of {killCount}
+          </div>
+          <div className="flex mt-2 font-light text-xl justify-center">
+            The chance that Moo has gotten this unlucky is {noDropRate}%.
+          </div>
+        </div>
+        <DownArrows />
       </div>
-      <div className="flex justify-center text-4xl">
-        Moo has a mole kill count of {killCount}
-      </div>
-      <div className="flex justify-center">
-        The chance that Moo has gotten this unlucky is {noDropRate}%
+      <div className="flex flex-col justify-center items-center text-center w-full h-screen space-y-4">
+        {/* <div></div> */}
+        <div>
+          <div className="text-xl font-light">
+            Moo usually gets about 60 kills per hour, which means that he has
+            spent
+          </div>
+          <div className="text-4xl">{hoursWasted} hours at mole.</div>
+        </div>
+
+        {/* <DownArrows /> */}
       </div>
     </div>
   );
